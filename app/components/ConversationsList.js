0a8fbe052806a03c5c1b4c45fb523bd17260dc02
Andrey <ghostmancer@gmail.com>
2016-12-04 10:31:04 -0800

    Cleanup

--- app/components/ConversationsList.js
+++ app/components/ConversationsList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, ListView, StyleSheet, Text } from 'react-native';
-import ConversationsRow from '../ConversationsRow';
+import ConversationsRow from './ConversationsRow';
 
 const styles = StyleSheet.create({
     container: {
---
+++ app/routes/Conversations.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ConversationsList from '../components/ConversationsList';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    }
+});
+
+export default class Conversations extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <ConversationsList></ConversationsList>
+            </View>
+        );
+    }
+}
