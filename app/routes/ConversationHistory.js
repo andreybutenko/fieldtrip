324345012de06e2f2f6883685edf76fd38c68963
Andrey <ghostmancer@gmail.com>
2016-12-09 15:49:22 -0800

    Cleanup conversation history

--- app/routes/ConversationHistory.js
+++ app/routes/ConversationHistory.js
@@ -17,60 +17,17 @@ const styles = StyleSheet.create({
 export default class ConversationHistory extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { messages: [], advancedChatHeight: 30 };
-        this.onSend = this.onSend.bind(this);
     }
     componentWillMount() {
         this.setState({
             messages: [
-                {
-                    _id: 1,
-                    text: 'Hello developer',
-                    createdAt: new Date(Date.UTC(2016, 8, 30, 17, 20, 0)),
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: 'https://facebook.github.io/react/img/logo_og.png',
-                    }
-                },
-                {
-                    _id: 2,
-                    text: 'Hello developer',
-                    createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
-                    user: {
-                        _id: 3,
-                        name: 'A',
-                        avatar: 'https://facebook.github.io/react/img/logo_og.png',
-                    }
-                },
-                {
-                    _id: 3,
-                    text: 'Hi!',
-                    createdAt: new Date(Date.UTC(2016, 9, 30, 17, 20, 0)),
-                    user: {
-                        _id: 1,
-                        name: 'Me',
-                        avatar: 'https://facebook.github.io/react/img/logo_og.png',
-                    }
-                }
             ]
         });
-    }
-    onSend(messages = []) {
-        this.setState((previousState) => {
-            return {
-                messages: GiftedChat.append(previousState.messages, messages),
-            };
-        });
-    }
-    onAdvancedChatUpdate(height) {
-        console.log(height)
-        this.setState({ advancedChatHeight: height });
     }
     render() {
         return (
             <View style={[styles.container, configStyles.sceneWrapper]}>
-                <View style={[styles.chat, {marginBottom: this.state.advancedChatHeight }]}>
+                <View style={styles.chat}>
                     <ChatBubble
                         fromMe={false}
                         sender={'Andrey Butenko'}
@@ -82,7 +39,7 @@ export default class ConversationHistory extends React.Component {
                         text={'Hello!'}
                     />
                 </View>
-                <AdvancedChat resizeCallback={(height) => this.onAdvancedChatUpdate(height)} />
+                <AdvancedChat />
             </View>
         );
     }
